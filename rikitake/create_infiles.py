5f98a95e935a0671e624a883f32b60c85288299e
Manuel Stocchi <manuel.stocchi@studio.unibo.it>
2020-05-28 12:14:56 +0200

    Implemented some exception handling.

--- rikitake/create_infiles.py
+++ rikitake/create_infiles.py
@@ -7,23 +7,44 @@ Created on Wed May 20 16:52:18 2020
 """
 
 import plumbum.cli.terminal as terminal
-
+import sys
+import os
 
 
 def read_input():
-    in_file=open("temp_for_create_infiles.txt")
+    try:
+       in_file=open(".temp_for_create_infiles.txt",'r')
+    except OSError:
+        print("Could not open '.temp_for_create_infiles.txt'. Maybe the file has been deleted?", file=sys.stderr)
+        sys.exit([3]) #could not find .temp_for_create_infiles.txt
     in_string=in_file.readlines()
-    assert len(in_string)==1
-    in_string=in_string[0]
-    split_string=in_string.split()
-    assert len(split_string)==7
+    try:
+      assert len(in_string)==1
+      in_string=in_string[0]
+      split_string=in_string.split()
+      assert len(split_string)==7
+    except AssertionError:
+        print("'.temp_for_create_infile.txt' file is not written as expected. Maybe writing process went wrong or the file has been modified.")
+        sys.exit([4]) #.temp_for_create_infile not as expected.
     return split_string
 
 def ask_for_data():
     while (True):
        in_data=terminal.prompt("Type the data in the form 'mu k N_steps x1_0 x2_0 y1_0 simulation_identifier")
-       mu=float(in_data.split()[0])
-       k=float(in_data.split()[1])
+       split_values=in_data.split()
+       if (len(split_values)!=7):
+           print("You did not gave 7 values. Try again.")
+           continue
+       try:
+          mu=float(in_data.split()[0])
+          k=float(in_data.split()[1])
+          N_steps=int(in_data.split()[2])
+          x1_0=float(in_data.split()[3])
+          x2_0=float(in_data.split()[4])
+          y1_0=float(in_data.split()[5])
+       except ValueError:
+          print("Some of the parameter values you gave are not numbers. Please try again.")
+          continue
        if (0<mu<10**2 and 1e-2<k<1e+2):
            break
        else:
@@ -33,7 +54,7 @@ def ask_for_data():
            if (go_on):
                break         
     
-    outfile=open("temp_for_create_infiles.txt",'w+')
+    outfile=open(".temp_for_create_infiles.txt",'w+')
     outfile.write(in_data)
     outfile.close()
     
@@ -43,6 +64,7 @@ def create_infile(save_dir):
     mu=in_data[0]
     k=in_data[1]
     N_steps=in_data[2]
+    """Creation of slightly different initial conditions."""
     initial_condition=[float(X) for X in in_data[3:6]]
     initial_condition1=[cond for cond in initial_condition]
     initial_condition1[0]+=2**-4
@@ -56,7 +78,9 @@ def create_infile(save_dir):
     for condition in initial_conditions:
         outstring=str(mu)+' '+str(k)+' '+str(N_steps)+' '+str(condition[0])+' '+str(condition[1])+' '+str(condition[2])+' '+identifier+'\n'
         outfile.write(outstring)
-    
-        
+    try:
+        os.remove(".temp_for_create_infiles.txt")
+    except FileNotFoundError:
+        print("Could not remove '.temp_for_create_infiles.txt'. Maybe the file has been deleted alright or you don't have permissions. The process will go on")
     
 

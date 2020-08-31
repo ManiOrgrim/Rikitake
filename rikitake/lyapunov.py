a73374b57811cd3b4c9a6b78e020ba895cec80b9
ManiOrgrim <manuel.stocchi@studio.unibo.it>
2020-08-31 11:54:19 +0200

    Improved docstrings

--- rikitake/lyapunov.py
+++ rikitake/lyapunov.py
@@ -16,11 +16,11 @@ from plumbum import colors
 def calculate_lyapunov(filename_1, filename_2,N_calc):
     """This function calculates the lyapunov exponent at each integration step between two solutions.
     PARAMETERS:
-        filename_1: string object with the file name in wich solution1 is stored
-        filename_2: string object with the file name in wich solution2 is stored
-        N_calc    : int between 1 and 3. Identifies the direction of the actual calculation.
+        filename_1: (string) String object with the file name in wich solution1 is stored
+        filename_2: (string) String object with the file name in wich solution2 is stored
+        N_calc    : (int) between 1 and 3. Identifies the direction of the actual calculation.
     RETURNS:
-        lyaps     : N_steps-dimension array-like containing the values of the lyapunov exponent calcuated between solution1 and solution2 at each integration step for."""
+        lyaps     : (float[N_steps]) N_steps-dimension array-like containing the values of the lyapunov exponent calcuated between solution1 and solution2 at each integration step for."""
     data_1, mu_1, k_1=read_data.get_data(filename_1,'2')
     data_2, mu_2, k_2=read_data.get_data(filename_2,'2')
     lenght=len(data_1)
@@ -47,11 +47,11 @@ def calculate_lyapunov(filename_1, filename_2,N_calc):
 def get_results(simulation_ID, save_dir):
     """This function leads the calculation of the lyapunov exponents of all the simulations.
     PARAMETERS:
-        simulation_ID: string with the simulation_ID.
-        save_dir     : path/to/dir in which previously calculated results are stored.
+        simulation_ID: (string) String with the simulation_ID.
+        save_dir     : (string) path/to/dir in which previously calculated results are stored.
     RETURNS:
-        results      : dim. 3 array-like containing values of lyapunov exponents beetween the unperturbed series and the perturbed ones. It is calcuted taking the mean value of the last 100 lyapunov exponents in each series.
-        lyaps        : dim. [3, N_steps] array-like containing the series of lyapunov exponent calculated for each step for each direction. """
+        results      : (float[3]) array-like containing values of lyapunov exponents beetween the unperturbed series and the perturbed ones. It is calcuted taking the mean value of the last 100 lyapunov exponents in each series.
+        lyaps        : (float[3, N_steps]) array-like containing the series of lyapunov exponent calculated for each step for each direction. """
     filename=[save_dir+'/'+simulation_ID+'_'+str(i)+'.csv' for i in range(4)]
     lyaps=[calculate_lyapunov(filename[0],filename[i], i) for i in range(1,4)]
     results=[]
@@ -64,11 +64,11 @@ def get_results(simulation_ID, save_dir):
 def plot_exponents(simulation_ID, save_dir):
     """This function plots the lyapunov exponents series. 
     PARAMETERS:
-        simulation_ID: string with the simulation_ID.
-        save_dir     : path/to/dir in which previously calculated results are stored.
+        simulation_ID: (string) String representing the simulation_ID.
+        save_dir     : (string) path/to/dir in which previously calculated results are stored.
     RETURNS:
-            fig      : figure object showing a plot of lyapunov exponents evolution for each direction
-            values   : dim. 3 array-like containing values of lyapunov exponents beetween the unperturbed series and the perturbed ones."""
+            fig      : (figure object) Figure showing a plot of lyapunov exponents evolution for each direction
+            values   : (float[3]) array-like containing values of lyapunov exponents beetween the unperturbed series and the perturbed ones."""
     fig, subplots = plt.subplots(1, 3, figsize=(12, 4))
     values, series=get_results(simulation_ID, save_dir)
     for i in range(3):
@@ -86,7 +86,7 @@ def lyapunov(save_dir):
    """This function leads all the process, calculating lyapunov exponents, creating the image and then saves it as 'simulation_ID_lyap_exp.png'
    Then saves the estimated lyapunov exponents in '<simulation_ID>_lyapunov_exp.dat' file.
    PARAMETERS:
-       save_dir   : path/to/dir in which previously calculated results are stored. Results of this process will be saved here.
+       save_dir   : (string) path/to/dir in which previously calculated results are stored. Results of this process will be saved here.
    RETURNS:
        TRUE       : if all the process ended without issues."""
    simulation_ID=read_data.get_simulation_ID(save_dir)   

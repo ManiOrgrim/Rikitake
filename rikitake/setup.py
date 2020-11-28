96ed2968b0ab6a401c7825ab835e5775b4f0584a
ManiOrgrim <manuel.stocchi@studio.unibo.it>
2020-11-28 17:13:31 +0100

    We are officially in v1.0.0

--- rikitake/setup.py
+++ rikitake/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'rikitake',
-    version = '0.1.0',
+    version = '1.0.0',
     packages = ['rikitake',],
     install_requires=[ 'plumbum', ],
     entry_points = {

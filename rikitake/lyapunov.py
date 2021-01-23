d729053c7c08d20b42d873e875006671159f986d
Manuel Stocchi <manuel.stocchi@studio.unibo.it>
2021-01-23 10:44:33 +0100

    Removed obsolete code chunks

--- rikitake/lyapunov.py
+++ rikitake/lyapunov.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+##!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Mon May 18 15:09:06 2020
@@ -46,63 +46,7 @@ def calculate_lyapunov(filename_1, filename_2,N_calc):
         
         lyaps[i]=cumulative_sum/i
         
-    return lyaps
-
-
-
-  
-def calculate_lyapunov_anal(filename_1):   #TODO: WORK HERE ON BETTER NAMES
-    data, mu, k =read_data.get_data(filename_1,'2')
-    A=mu*(k**2-k**-2)
-    length=len(data)
-    def row1 (mu, A, x1, x2, y1):
-        return [-mu, y1, x2]
-    def row2 (mu, A, x1, x2, y1):
-        return [y1-A, -mu, -A*x1]
-    def row3 (mu, A, x1, x2, y1):
-        return [-x2,-x1,0]
-        
-    def calculate_jacobian (mu,A, x1,x2, y1):
-        jacobian=np.empty([3,3])
-        jacobian[0]=np.array(row1(mu,A, x1,x2, y1))
-        jacobian[1]=np.array(row2(mu,A, x1,x2, y1))
-        jacobian[2]=np.array(row3(mu,A, x1,x2, y1))
-        return jacobian
-    lyap1=[]
-    lyap2=[]
-    lyap3=[]
-    for state in data:
-        x1=state[0]
-        x2=state[1]
-        y1=state[2]
-        jacobian=calculate_jacobian(mu,A, x1,x2,y1)
-        eigenvalues, eigenvectors=np.linalg.eig(jacobian)
-        lyap1.append(np.log(abs(eigenvalues[0].real)))
-        lyap2.append(np.log(abs(eigenvalues[1].real)))
-        lyap3.append(np.log(abs(eigenvalues[2].real)))
-    lyap1=np.array(lyap1)
-    lyap2=np.array(lyap2)
-    lyap3=np.array(lyap3)
-    mean_lyap1=np.empty(length)
-    mean_lyap2=np.empty(length)
-    mean_lyap3=np.empty(length)
-    cumulative1=0
-    cumulative2=0
-    cumulative3=0
-    for i in range(length):
-     cumulative1+=lyap1[i]
-     cumulative2+=lyap2[i]
-     cumulative3+=lyap3[i]
-     mean_lyap1[i]=cumulative1/i
-     mean_lyap2[i]=cumulative2/i
-     mean_lyap3[i]=cumulative3/i 
-    
-    return mean_lyap1, mean_lyap2, mean_lyap3
-
-
-
-
-        
+    return lyaps        
 
 
 
@@ -144,30 +88,13 @@ def plot_exponents(simulation_ID, save_dir):
     return fig, values
 
 
-def plot_exponents_anal(simulation_ID, save_dir):
-    """This function plots the lyapunov exponents series. 
-    PARAMETERS:
-        simulation_ID: (string) String representing the simulation_ID.
-        save_dir     : (string) path/to/dir in which previously calculated results are stored.
-    RETURNS:
-            fig      : (figure object) Figure showing a plot of lyapunov exponents evolution for each direction
-            values   : (float[3]) array-like containing values of lyapunov exponents beetween the unperturbed series and the perturbed ones."""
-    fig, subplots = plt.subplots(1, 3, figsize=(12, 4))
-    series=calculate_lyapunov_anal(simulation_ID+"_0.csv")
-    for i in range(3):
-        subplots[i].plot(series[i], color='r')
-        subplots[i].set_xlabel("integration steps")
-        subplots[i].set_ylabel("Lyapuniov exponent estimate")
-        #subplots[i].hlines(values[i], 0, len(series[i]), label=str(round(values[i],2)), linestyle='dashed')
-        subplots[i].legend()
-    subplots[0].set_title('exponent 1')
-    subplots[1].set_title('exponent 2')
-    subplots[2].set_title('exponent 3')
-    return fig
 
 def lyapunov(save_dir):
-   """This function leads all the process, calculating lyapunov exponents, creating the image and then saves it as 'simulation_ID_lyap_exp.png'
-   Then saves the estimated lyapunov exponents in '<simulation_ID>_lyapunov_exp.dat' file.
+   """This function leads all the process, 
+   calculating lyapunov exponents, creating the image and 
+   then saves it as 'simulation_ID_lyap_exp.png'
+   Then saves the estimated lyapunov exponents in 
+   '<simulation_ID>_lyapunov_exp.dat' file.
    PARAMETERS:
        save_dir   : (string) path/to/dir in which previously calculated results are stored. Results of this process will be saved here.
    RETURNS:

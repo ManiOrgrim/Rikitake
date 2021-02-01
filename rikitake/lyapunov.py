6228c4c7c094fe5720230d460ff0b701f57a0db1
Manuel Stocchi <manuel.stocchi@studio.unibo.it>
2021-02-01 17:15:00 +0100

    Iproved docstrings

--- rikitake/lyapunov.py
+++ rikitake/lyapunov.py
@@ -13,7 +13,7 @@ from plumbum import colors
 
 
 
-def calculate_lyapunov(filename_1, filename_2,N_calc):
+def calculate_lyapunov(filename_1, filename_2,N_calc):  #TODO: remove references to N_calc
     """This function calculates the lyapunov exponent at each integration step between two solutions.
     PARAMETERS:
         filename_1: (string) String object with the file name in wich solution1 is stored
@@ -32,18 +32,23 @@ def calculate_lyapunov(filename_1, filename_2,N_calc):
     except AssertionError:
         print(colors.red|"The integration results are not written as expexted", file=sys.stderr)
         sys.exit([6]) #Integration results are not written as expected
-    difference=data_1-data_2
-    norms=np.linalg.norm(difference, axis=1) #TODO: work on better names
-    norm_0=norms[0]
-    log_diff_ratio=np.log(norms/norm_0)
+    """ Actual calculation of the Lyapunov exponents"""
+    
+    """Difference between the two simulations"""
+    difference=data_1-data_2                        
+    """Norms of the difference vectors"""
+    norms=np.linalg.norm(difference, axis=1)
+    """Norm of the initial difference"""
+    norm_0=norms[0]  
+    """Calculating the ln of the ratio between evolved state and initial state"""                               
+    log_diff_ratio=np.ln(norms/norm_0)
     lyaps=np.empty(lenght)
     cumulative_sum=0 ####
     
     print("Lyapunov exponents calculation:", N_calc, "/1")
     for i in Progress.range(1, lenght):
+        """calculating the mean of the ratios from initial state to state i"""
         cumulative_sum+=log_diff_ratio[i]
-        
-        
         lyaps[i]=cumulative_sum/i
         
     return lyaps        
@@ -57,16 +62,16 @@ def get_results(simulation_ID, save_dir):
         simulation_ID: (string) String with the simulation_ID.
         save_dir     : (string) path/to/dir in which previously calculated results are stored.
     RETURNS:
-        results      : (float[3]) array-like containing values of lyapunov exponents beetween the unperturbed series and the perturbed ones. It is calcuted taking the mean value of the last 100 lyapunov exponents in each series.
-        lyaps        : (float[3, N_steps]) array-like containing the series of lyapunov exponent calculated for each step for each direction. """
+        mean_lyap     : (float) array-like containing values of lyapunov exponents beetween the unperturbed series and the perturbed one. 
+                        It is calcuted taking the mean value of the last 100 lyapunov exponents.
+        lyaps        : (float[N_steps]) array-like containing the series of lyapunov exponent calculated for each timestep. """
     filename=[save_dir+'/'+simulation_ID+'_'+str(i)+'.csv' for i in range(2)]
     lyaps=calculate_lyapunov(filename[0],filename[1], 1)
-    results=[]
-    
+    """Calculating the mean of the last 100 lyapunov exponents 
+    This is an indication of the actual exponent value"""
     last_100lyap=lyaps[-100:]
     mean_lyap=np.mean(last_100lyap)
-    results.append(mean_lyap)
-    return results, lyaps
+    return mean_lyap, lyaps
 
 def plot_exponents(simulation_ID, save_dir):
     """This function plots the lyapunov exponents series. 
@@ -74,24 +79,24 @@ def plot_exponents(simulation_ID, save_dir):
         simulation_ID: (string) String representing the simulation_ID.
         save_dir     : (string) path/to/dir in which previously calculated results are stored.
     RETURNS:
-            fig      : (figure object) Figure showing a plot of lyapunov exponents evolution for each direction
-            values   : (float[3]) array-like containing values of lyapunov exponents beetween the unperturbed series and the perturbed ones."""
+            fig      : (figure object) Figure showing a plot of lyapunov exponents evolution fo
+            values   : (float) array-like containing values of lyapunov exponents beetween the unperturbed series and the perturbed ones."""
     fig, subplot = plt.subplots(1, 1, figsize=(4, 4))
-    values, series=get_results(simulation_ID, save_dir)
+    mean_lyap, series=get_results(simulation_ID, save_dir)
     
     subplot.plot(series, color='r')
     subplot.set_xlabel("integration steps")
     subplot.set_ylabel("Lyapuniov exponent estimate")
-    subplot.hlines(values[0], 0, len(series), label=str(round(values[0],2)), linestyle='dashed')
+    subplot.hlines(mean_lyap, 0, len(series), label=str(round(mean_lyap,2)), linestyle='dashed')
     subplot.legend()
     subplot.set_title('Greatest Lyapunov exponent')
-    return fig, values
+    return fig, mean_lyap
 
 
 
 def lyapunov(save_dir):
    """This function leads all the process, 
-   calculating lyapunov exponents, creating the image and 
+   calculating lyapunov exponent, creating the image and 
    then saves it as 'simulation_ID_lyap_exp.png'
    Then saves the estimated lyapunov exponents in 
    '<simulation_ID>_lyapunov_exp.dat' file.
@@ -101,9 +106,8 @@ def lyapunov(save_dir):
        TRUE       : if all the process ended without issues."""
    
    simulation_ID=read_data.get_simulation_ID(save_dir)   
-   fig, exp_values=plot_exponents(simulation_ID, save_dir)
+   fig, mean_lyap=plot_exponents(simulation_ID, save_dir)
    fig.savefig(save_dir+'/'+simulation_ID+"_lyap_exp.png", bbox_inches='tight')
    open_file=open(simulation_ID+'lyap.dat','w+')
-   for exp in exp_values:
-       open_file.write(str(exp)+' ')
+   open_file.write(str(mean_lyap)+' ')
    return True
